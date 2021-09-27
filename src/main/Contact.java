6c19b83b63ef208d84f793fff2efed64514f589f
ferbrett <brettferguson1998@gmail.com>
2021-09-26 21:04:18 -0500

    Added Contact object and CRUD Functions
    Created a contact object in the Contact class and added CRUD functions in the ContactService class based on the requirements provided.

--- src/main/Contact.java
+++ src/main/Contact.java
@@ -2,4 +2,50 @@ package main;
 
 public class Contact {
 
+	// Contact object
+	public void contact(String id, String firstName, String lastName, 
+			String phoneNumber, String address) {
+		
+		boolean enteredValue = true;
+		
+		if (id.length() > 10) {
+			System.out.println("This ID is too long. Please enter another ID under 10 characters: ");
+		} else if (enteredValue == false) {
+			System.out.println("You must enter a value here.");
+		} else {
+			System.out.println("ID accepted.");
+		}
+		
+		if (firstName.length() > 10) {
+			System.out.println("This first name is too long. Please enter another first name under 10 characters: ");
+		} else if (enteredValue == false) {
+			System.out.println("You must enter a value here.");
+		} else {
+			System.out.println("First name accepted.");
+		}
+		
+		if (lastName.length() > 10) {
+			System.out.println("This last name is too long. Please enter another last name under 10 characters: ");
+		} else if (enteredValue == false) {
+			System.out.println("You must enter a value here.");
+		} else {
+			System.out.println("Last name accepted.");
+		}
+		
+		if (phoneNumber.length() > 10) {
+			System.out.println("This phone number is too long. Please enter another phone number that is exactly 10 characters: ");
+		} else if (enteredValue == false) {
+			System.out.println("You must enter a value here.");
+		} else {
+			System.out.println("Phone number accepted.");
+		}
+		
+		if (address.length() > 30) {
+			System.out.println("This address is too long. Please enter another address under 30 characters: ");
+		} else if (enteredValue == false) {
+			System.out.println("You must enter a value here.");
+		} else {
+			System.out.println("Address accepted.");
+		}
+	}
 }
--- src/main/ContactService.java
+++ src/main/ContactService.java
@@ -1,5 +1,56 @@
 package main;
 
-public class ContactService {
+import main.Contact;
 
+public class ContactService extends Contact {
+
+	public String addContactId(String id) {
+		
+		return id;
+	}
+	
+	public String deleteContactId(String id) {
+		
+		return id;
+	}
+	
+	public String addFirstName(String firstName) {
+		
+		return firstName;
+	}
+	
+	public String updateFirstName(String firstName) {
+		
+		return firstName;
+	}
+	
+	public String addLastName(String lastName) {
+		
+		return lastName;
+	}
+	
+	public String updateLastName(String lastName) {
+		
+		return lastName;
+	}
+	
+	public String addPhoneNumber(String phoneNumber) {
+		
+		return phoneNumber;
+	}
+	
+	public String updatePhoneNumber(String phoneNumber) {
+		
+		return phoneNumber;
+	}
+	
+	public String addAddress(String address) {
+		
+		return address;
+	}
+	
+	public String updateAddress(String address) {
+		
+		return address;
+	}
 }
