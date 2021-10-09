917dfeb64ce759d045dba0bcebac35055351a703
ferbrett <brettferguson1998@gmail.com>
2021-10-09 09:46:42 -0500

    Contact.java Update
    Completed a redesign of the Contact.java file.

--- src/main/Contact.java
+++ src/main/Contact.java
@@ -2,50 +2,152 @@ package main;
 
 public class Contact {
 
-	// Contact object
-	public void contact(String id, String firstName, String lastName, 
+	/*
+	 * Reusable constant/variable declarations/initializations
+	 */
+	private static final int CONTACT_PHONENUMBER_LENGTH = 10;
+	private static final byte CONTACT_ID_LENGTH = 10;
+	private static final byte CONTACT_FIRSTNAME_LENGTH = 10;
+	private static final byte CONTACT_LASTNAME_LENGTH = 10;
+	private static final byte CONTACT_ADDRESS_LENGTH = 30;
+	
+	private static final String INITIALIZER = "INITIAL";
+	private static final String INITIALIZER_NUM = "1235559999";
+	
+	private String contactId;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String address;
+	
+	Contact() {
+		this.contactId = INITIALIZER;
+		this.firstName = INITIALIZER;
+		this.lastName = INITIALIZER;
+		this.phoneNumber = INITIALIZER;
+		this.address = INITIALIZER;
+	}
+	
+	Contact(String contactId) {
+		updateContactId(contactId);
+		this.firstName = INITIALIZER;
+		this.lastName = INITIALIZER;
+		this.phoneNumber = INITIALIZER;
+		this.address = INITIALIZER;
+	}
+	
+	Contact(String contactId, String firstName) {
+		updateContactId(contactId);
+		updateFirstName(firstName);
+		this.lastName = INITIALIZER;
+		this.phoneNumber = INITIALIZER;
+		this.address = INITIALIZER;
+	}
+	
+	Contact(String contactId, String firstName, String lastName) {
+		updateContactId(contactId);
+		updateFirstName(firstName);
+		updateLastName(lastName);
+		this.phoneNumber = INITIALIZER;
+		this.address = INITIALIZER;
+	}
+	
+	Contact(String contactId, String firstName, String lastName,
+			String phoneNumber) {
+		updateContactId(contactId);
+		updateFirstName(firstName);
+		updateLastName(lastName);
+		updatePhoneNumber(phoneNumber);
+		this.address = INITIALIZER;
+	}
+	
+	Contact(String contactId, String firstName, String lastName,
 			String phoneNumber, String address) {
-		
-		boolean enteredValue = true;
-		
-		if (id.length() > 10) {
-			System.out.println("This ID is too long. Please enter another ID under 10 characters: ");
-		} else if (enteredValue == false) {
-			System.out.println("You must enter a value here.");
+		updateContactId(contactId);
+		updateFirstName(firstName);
+		updateLastName(lastName);
+		updatePhoneNumber(phoneNumber);
+		updateAddress(address);
+	}
+	
+	private final String getContactId() {
+		return contactId;
+	}
+	
+	private final String getFirstName() {
+		return firstName;
+	}
+	
+	private final String getLastName() {
+		return lastName;
+	}
+	
+	private final String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	private final String getAddress() {
+		return address;
+	}
+	
+	protected void updateFirstName(String firstName) {
+		if (firstName == null) {
+			throw new IllegalArgumentException("First name can't be empty.");
+		} else if (firstName.length() > CONTACT_FIRSTNAME_LENGTH) {
+			throw new IllegalArgumentException("First name can't be longer than " +
+												CONTACT_FIRSTNAME_LENGTH + " characters.");
 		} else {
-			System.out.println("ID accepted.");
+			this.firstName = firstName;
 		}
-		
-		if (firstName.length() > 10) {
-			System.out.println("This first name is too long. Please enter another first name under 10 characters: ");
-		} else if (enteredValue == false) {
-			System.out.println("You must enter a value here.");
+	}
+	
+	protected void updateLastName(String lastName) {
+		if (lastName == null) {
+			throw new IllegalArgumentException("Last name can't be empty.");
+		} else if (lastName.length() > CONTACT_LASTNAME_LENGTH) {
+			throw new IllegalArgumentException("Last name can't be loger than " +
+												CONTACT_LASTNAME_LENGTH + " characters");
 		} else {
-			System.out.println("First name accepted.");
+			this.lastName = lastName;
 		}
-		
-		if (lastName.length() > 10) {
-			System.out.println("This last name is too long. Please enter another last name under 10 characters: ");
-		} else if (enteredValue == false) {
-			System.out.println("You must enter a value here.");
+	}
+	
+	protected void updatePhoneNumber(String phoneNumber) {
+		String allowed = "[0-9]+";
+		if (phoneNumber == null) {
+			throw new IllegalArgumentException("Phone number can't be empty.");
+		} else if (phoneNumber.length() != CONTACT_PHONENUMBER_LENGTH) {
+			throw new IllegalArgumentException(
+					"Phone number length is invalid. Ensure it is " +
+					CONTACT_PHONENUMBER_LENGTH + " digits.");
+		} else if (!phoneNumber.matches(allowed)) {
+			throw new IllegalArgumentException(
+					"Phone number can't have any characters except numbers.");
 		} else {
-			System.out.println("Last name accepted.");
+			this.phoneNumber = phoneNumber;
 		}
-		
-		if (phoneNumber.length() > 10) {
-			System.out.println("This phone number is too long. Please enter another phone number that is exactly 10 characters: ");
-		} else if (enteredValue == false) {
-			System.out.println("You must enter a value here.");
+	}
+	
+	protected void updateAddress(String address) {
+		if (address == null) {
+			throw new IllegalArgumentException("Address can't be empty.");
+		} else if (address.length() > CONTACT_ADDRESS_LENGTH) {
+			throw new IllegalArgumentException("Address can't be longer than " +
+												CONTACT_ADDRESS_LENGTH +
+												" characters.");
 		} else {
-			System.out.println("Phone number accepted.");
+			this.address = address;
 		}
-		
-		if (address.length() > 30) {
-			System.out.println("This address is too long. Please enter another address under 30 characters: ");
-		} else if (enteredValue == false) {
-			System.out.println("You must enter a value here.");
+	}
+	
+	protected void updateContactId(String contactId) {
+		if (contactId == null) {
+			throw new IllegalArgumentException("Contact ID can't be empty.");
+		} else if (address.length() > CONTACT_ID_LENGTH) {
+			throw new IllegalArgumentException("Contact ID can't be longer than " +
+												CONTACT_ID_LENGTH + " characters.");
 		} else {
-			System.out.println("Address accepted.");
+			this.contactId = contactId;
 		}
 	}
 }
--- src/main/ContactService.java
+++ src/main/ContactService.java
@@ -1,56 +1,5 @@
 package main;
 
-import main.Contact;
+public class ContactService {
 
-public class ContactService extends Contact {
-
-	public String addContactId(String id) {
-		
-		return id;
-	}
-	
-	public String deleteContactId(String id) {
-		
-		return id;
-	}
-	
-	public String addFirstName(String firstName) {
-		
-		return firstName;
-	}
-	
-	public String updateFirstName(String firstName) {
-		
-		return firstName;
-	}
-	
-	public String addLastName(String lastName) {
-		
-		return lastName;
-	}
-	
-	public String updateLastName(String lastName) {
-		
-		return lastName;
-	}
-	
-	public String addPhoneNumber(String phoneNumber) {
-		
-		return phoneNumber;
-	}
-	
-	public String updatePhoneNumber(String phoneNumber) {
-		
-		return phoneNumber;
-	}
-	
-	public String addAddress(String address) {
-		
-		return address;
-	}
-	
-	public String updateAddress(String address) {
-		
-		return address;
-	}
 }
